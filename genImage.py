67c0d6db8e67a5ed2c3f3e6e622d45324834387e
CreepyChameleon <brendanalm3@gmail.com>
2022-05-11 17:42:58 -0400

    finish image generation

--- genImage.py
+++ genImage.py
@@ -3,9 +3,24 @@ import os
 
 cwd = os.getcwd()
 
-def make_image(date="2020 09 21", day="A"):
-    img = Image.open(cwd + "images/.png")
+def make_image(number='A', suite="spade"):
+    img = Image.open(cwd + "/resources/card.png")
+    d = ImageDraw.Draw(img)
+    fnt = ImageFont.truetype(cwd + "/resources/Futura-Medium.ttf", 50)
+    suiteImg = Image.open(cwd + f"/resources/{suite}.png").resize((32, 32))
+
+    if suite in ["heart", "diamond"]:
+        txtColor = (255, 0, 0)
+    else:
+        txtColor = (0, 0, 0)
+
+    d.text((20, 10), number, fill=txtColor, font=fnt)
     
+
+    img.paste(suiteImg, (22,75), suiteImg)
+    d.text((300, 395), number, fill=txtColor, font=fnt)
+    img.paste(suiteImg, (302,460), suiteImg)
+
     img.show()
     # img.save('img.png')
 make_image()
